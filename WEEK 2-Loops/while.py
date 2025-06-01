37b75c5ef0216cc823175c7c0477539c2367aa7e
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-06-01 11:10:28 +0300

    Create while.py

---
+++ WEEK 2-Loops/while.py
@@ -0,0 +1,18 @@
+#poor design
+'''
+print("meow")
+print("meow")
+print("meow")
+is bad design because it repeating itself
+'''
+#Loop
+'''i = 3
+while i != 0:
+    print("meow")
+and this will print meow forever
+because it`s always i is not equal 0
+'''
+i = 0
+while i < 3:
+    print("meow")
+    i += 1 
